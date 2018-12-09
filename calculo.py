efa3659495b33cb396bce311a81e116ff0a97c81
Jéssica Magalhães Mourão <38639511+jemalicisou@users.noreply.github.com>
2018-12-09 17:59:02 -0200

    Pequeno menu de opções adicionado

--- calculo.py
+++ calculo.py
@@ -19,3 +19,28 @@ c = (10.5)
 
 u= a * a -b + c
 print (u)
+
+
+#Criando um pequeno menu de opções 
+
+# Escolha a opção abaixo
+
+print( "Menu :\n1 =Escreva Guilheme \n2 = Escreva João \n3 = Escreva Luisa \n4 =Escreva Cristina")
+
+opcao = input("Escolha uma opção: ")
+
+
+if opcao == '1':
+    print('Guilherme ')
+
+elif opcao == '2':
+    print('João ')
+
+elif opcao == '3':
+    print('Luisa ')
+
+elif opcao == '4':
+    print('Cristina')
+else:
+    print( 'Opção inválida - Selecione um número de 1 a 41')
+
